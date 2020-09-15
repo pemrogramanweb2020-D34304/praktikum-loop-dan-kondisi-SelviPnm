c7480ef693fa192a8177823bd172a2ddf5066f53
SelviPnm <70251654+SelviPnm@users.noreply.github.com>
2020-09-15 11:02:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<html> 
+    <head> 
+        <title>Bintang Piramida</title> 
+    </head> 
+    <body> 
+    <h1>Bintang Piramida</h1> 
+      
 <?php
-
+for ($a = 1; $a <= 5; $a++){
+        for($b = 5; $b >= $a; $b--){
+        echo"*";
+    }
+        echo"<p>";
+    }
 ?>
+  </body> 
+  </html>
